d85303a9c01efc97333bbef453d529f0ef51f2a6
Manish Ranjan <manish24online@gmail.com>
2013-03-27 09:30:33 -0700

    added

--- careerbook/Model/singleton.php
+++ careerbook/Model/singleton.php
@@ -11,7 +11,7 @@ class DBConnection {
 	private static $instance;
 	private static $_host = "localhost";
 	private static $_user = "root";
-	private static $_password = "";
+	private static $_password = "root";
 	private static $_database = "careerbook";
 	private $_tableName = "";
 	private $_join = "";
